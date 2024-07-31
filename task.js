9050a20d0df8ee32e1ba7ae7a5e5db942aa6c64a
nikuusyaa <nika.borozen@gmail.com>
2024-07-31 09:50:11 +0200

    created task.js and added an input

---
+++ task.js
@@ -0,0 +1,55 @@
+var input =[
+  {
+    tickets: 6,
+    chapters: [
+      {
+        tickets: 0,
+        chapters: [
+          {
+            tickets: 1,
+            chapters: [],
+          },
+          {
+            tickets: 1,
+            chapters: [],
+          },
+        ],
+      },
+      {
+        tickets: 0,
+        chapters: [],
+      },
+      {
+        tickets: 4,
+        chapters: [],
+      },
+    ],
+  },
+  {
+    tickets: 0,
+    chapters: [
+      {
+        tickets: 4,
+        chapters: [],
+      },
+      {
+        tickets: 0,
+        chapters: [
+          {
+            tickets: 5,
+            chapters: [
+              {
+                tickets: 2,
+                chapters: [],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        tickets: 3,
+        chapters: [],
+      },
+    ],
+  },
+];
