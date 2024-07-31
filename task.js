7030081d0478a76516a5d2f26964316506192f63
nikuusyaa <nika.borozen@gmail.com>
2024-07-31 10:28:15 +0200

    created a function for calculating , called the function, formatted back to JSON

--- task.js
+++ task.js
@@ -1,4 +1,4 @@
-var input =[
+var input = [
   {
     tickets: 6,
     chapters: [
@@ -53,3 +53,21 @@ var input =[
     ],
   },
 ];
+//function for calculating total number of tickets in one chapter
+function CalculateTickets(chapter) {
+  let totalTickets = chapter.tickets;
+
+  //going through each chapter in input
+  for (const subchapter of chapter.chapters) {
+    //recursively calling the CalculateTickets function for each subchapter
+    totalTickets += CalculateTickets(subchapter);
+  }
+  chapter.total = totalTickets;
+  return totalTickets;
+}
+
+// calling CalculateTickets for each elemt of the input
+input.forEach((chapter) => CalculateTickets(chapter));
+
+//formatting back to JSON string
+console.log(JSON.stringify(input, null, 2));
